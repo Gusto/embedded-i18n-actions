29448bd4a1760ea79afc159bde3a279b3406bb33
Wil Alvarez <wil.alvarez@gusto.com>
2025-01-28 16:32:53 -0800

    Fix path for push_source script and lokalise CLI. Add more validations to the script. Include a test workflow

--- .github/actions/push_sources/push_sources.sh
+++ .github/actions/push_sources/push_sources.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
 ROOT_DIR=$(pwd)
-
+LOKALISE_CLI_PATH="${ROOT_DIR}/bin/lokalise2"
 # We want to transform the FILE_PATTERN string from the action input into an array,
 # that way git commands parse each pattern correctly as a separate argument.
 IFS=' ' read -ra FILE_PATTERNS <<< "$FILE_PATTERNS"
-echo "Finding locale files - root: $ROOT_DIR"
+
+echo "ROOT_DIR: $ROOT_DIR"
+echo "DRY_RUN: $DRY_RUN"
+
+validate_lokalise_bin() {
+  if [ ! -f "$LOKALISE_CLI_PATH" ]
+  then
+    echo "ERROR: Lokalise binary couldn't be found: $LOKALISE_CLI_PATH"
+    exit 1
+  fi
+}
 
 push_to_lokalise() {
   local file=$1
   echo "Pushing file: $file"
-  ./bin/lokalise2 --token "${LOKALISE_API_TOKEN}" \
+  if [ "$DRY_RUN" == "true" ]
+  then
+    return
+  fi
+
+  "${LOKALISE_CLI_PATH}" --token "${LOKALISE_API_TOKEN}" \
   --project-id "${LOKALISE_PROJECT_ID}" \
   file upload --file="${file}" \
   --lang-iso en \
@@ -26,9 +41,7 @@ process_files() {
 
   # Navigate to the directory passed as argument
   pushd "$dir" > /dev/null || exit 1
-  echo "===="
-  echo "Searching files in directory: $dir"
-  echo "===="
+  echo "== Searching for files in $dir =="
 
   local files
   files=$(
@@ -40,25 +53,36 @@ process_files() {
     fi | awk NF
   )
 
-  while read -r file; do
-    push_to_lokalise "$file"
-  done < <(echo "${files[@]}")
+  if [ -z "${files[0]}" ]
+  then
+    echo "No files found"
+  else
+    while read -r file; do
+      push_to_lokalise "$file"
+    done < <(echo "${files[@]}")
+  fi
 
   # Return to the previous directory
   popd > /dev/null || exit 1
 }
 
+# ===
+# Here's where the actual logic starts
+# ===
+
 # We want to make sure all directories exist before we push anything to Lokalise
-for path in "${LOCALE_PATHS[@]}"; do
+while read -r path; do
   if [ ! -d "$path" ]; then
-    echo "Directory '$path' not found! Cannot process files."
+    echo "ERROR: Directory '$path' not found! Cannot process files."
     exit 1
   fi
-done
+done < <(echo "${LOCALE_PATHS[@]}" | awk NF)
 
-for path in "${LOCALE_PATHS[@]}"; do
+validate_lokalise_bin
+
+while read -r path; do
   process_files "$path"
-done
+done < <(echo "${LOCALE_PATHS[@]}" | awk NF)
 
 # Ensure we're back at the root directory
 cd "$ROOT_DIR" || exit 1
